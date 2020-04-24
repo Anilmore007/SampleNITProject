d7c92737549ce39993b94159ecf58d54334d526c
Dev2 <Dev2@gmail.com>
2020-04-24 12:38:45 +0530

    dev1 modified Test.java

--- GitRepoProject/src/in/nit/servlet/Test.java
+++ GitRepoProject/src/in/nit/servlet/Test.java
@@ -1,5 +1,13 @@
 package in.nit.servlet;
 
+import in.nit.hibernate.Utility;
+
 public class Test {
+	
+	public static void main(String[] args) {
+		
+		Utility u=new Utility();
+		System.out.println(u.getSysDate());
+	}
 
 }
